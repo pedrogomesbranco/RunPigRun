c0b900e3f4c7b4547a7b79394f8125bf45aec4e4
João Pereira <jpereira@Joao-Pereiras-MacBook-Pro.local>
2016-11-23 18:47:50 -0200

    Fixed project structure

---
+++ PigRunner/Classes/Obstacles/SpinningWheel.swift
@@ -0,0 +1,13 @@
+//
+//  SpinningWheel.swift
+//  PigRunner
+//
+//  Created by Joao Pereira on 23/11/16.
+//  Copyright © 2016 João Pereira. All rights reserved.
+//
+
+import SpriteKit
+
+class SpinningWheel: SKSpriteNode {
+    
+}
