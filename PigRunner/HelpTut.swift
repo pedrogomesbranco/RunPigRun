88bf1d09b22cada1f4e4e34e0d2e818e4db7f5f6
pedrogomesbranco <pedrogomesbranco@me.com>
2017-03-17 13:46:34 -0300

    imagens puff e cozinheiro com tutorial funcionando

---
+++ PigRunner/HelpTut.swift
@@ -0,0 +1,24 @@
+//
+//  RankingViewController.swift
+//  PigRunner
+//
+//  Created by Gustavo Fonseca Olenka on 02/03/17.
+//  Copyright © 2017 João Pereira. All rights reserved.
+//
+
+import UIKit
+import FBSDKCoreKit
+
+class HelpTut: UIViewController{
+    
+    var menuScene: MenuScene!
+    @IBOutlet weak var returnButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func didTapButton(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: {})
+    }
+}
